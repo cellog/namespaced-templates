338fe3eaf6989279d60ce5fb427094bb265c2318
Gregory Beaver <greg@chiaraquartet.net>
2015-06-01 17:27:33 -0500

    remove documentation, pulls from README on github

--- package.js
+++ package.js
@@ -5,9 +5,6 @@ Package.describe({
   summary: 'Namespaced spacebars templates reduce work and allow greater flexibility',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/cellog/namespaced-templates',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
